68edc5defff6c096027d3a350009f359de228652
Van Ni <vanni4018@gmail.com>
2024-02-28 20:48:51 +0700

    Retry connection attempts with increasing intervals to avoid overwhelming the database server in case of temporary issues.

--- utils/db.ts
+++ utils/db.ts
@@ -3,13 +3,15 @@ import { env } from '../config/enviroment';
 
 const MONGO_URI = env.MONGODB_URL || ''; // Placeholder URI, replace with your actual connection string
 
-export async function connectToMongoose(): Promise<mongoose.Mongoose> {
+export async function connectToMongoose() {
     try {
         await mongoose.connect(MONGO_URI);
         console.log('Connected to MongoDB with Mongoose');
         return mongoose;
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-        throw error; // Re-throw the error to be handled by the calling code
+        setTimeout(() => {
+            connectToMongoose(); // Retry after a delay
+        }, 5000); // Initial retry delay of 5 seconds (adjustable)
     }
 }
