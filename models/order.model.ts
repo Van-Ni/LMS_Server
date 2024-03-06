f5934b4d6952089e9a5d096dc59df9fbad2c820c
Van Ni <vanni4018@gmail.com>
2024-03-06 21:00:12 +0700

    Notification and Order model

---
+++ models/order.model.ts
@@ -0,0 +1,25 @@
+import { Schema, Document, model } from 'mongoose';
+
+export interface IOrder extends Document {
+    courseId: string;
+    userId: string;
+    payment_info: object;
+}
+
+const orderSchema = new Schema<IOrder>({
+    courseId: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    },
+    payment_info: {
+        type: Object
+    }
+})
+
+const orderModel = model<IOrder>('Order', orderSchema);
+
+export default orderModel;
