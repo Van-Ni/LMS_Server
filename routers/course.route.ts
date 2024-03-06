f96e96bdb9561464aab46d68ec0df4a6b53bf7b1
Van Ni <vanni4018@gmail.com>
2024-03-06 14:56:59 +0700

    Create question, add reply in question

--- routers/course.route.ts
+++ routers/course.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { authorziteRoles, isAuthenticated } from "../middlewares/auth";
 import { upload } from "../utils/image";
 import { courseController } from "../controllers/course.controller";
+import { questionController } from "../controllers/question.controller";
 
 
 
@@ -16,6 +17,9 @@ Router.route('/:id')
     .get(courseController.getSingleCourse)
 
 Router.get("/get-course-content/:id", isAuthenticated, courseController.getCourseByUser)
+Router.post("/add-question", isAuthenticated, questionController.addQuestion)
+Router.post("/add-answer", isAuthenticated, questionController.addAnswer)
+
 
 
 export const courseRoute = Router;
