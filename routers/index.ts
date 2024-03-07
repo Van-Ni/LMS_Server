29af79b5125f84a0561eae23deec724d8c536b7c
Van Ni <vanni4018@gmail.com>
2024-03-07 11:01:41 +0700

    Create order

--- routers/index.ts
+++ routers/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoute } from "./user.route";
 import { courseRoute } from "./course.route";
+import { orderRoute } from "./order.route";
 
 
 
@@ -9,5 +10,6 @@ const Router = express.Router();
 // Board APIs
 Router.use("/api/user", userRoute)
 Router.use("/api/course", courseRoute)
+Router.use("/api/order", orderRoute)
 
 export const API_V1 = Router;
